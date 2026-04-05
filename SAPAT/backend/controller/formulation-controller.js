2089011df1f6c5766d9f9fff882fe5ad7bcd4765
Mar_Paul Palacio <mrpalacio@up.edu.ph>
2026-04-05 17:16:23 +0800

    fix: changed timeframe

--- SAPAT/backend/controller/formulation-controller.js
+++ SAPAT/backend/controller/formulation-controller.js
@@ -25,6 +25,7 @@ const createFormulation = async (req, res) => {
             fat_content:fat_protein_content, lactating_phase: is_lactating, pregnant_phase: months_pregnant, origNutrientTargets: nutrients
         });
 
+        
         const filteredFormulation = {
             "_id": newFormulation._id,
             "code": code,
